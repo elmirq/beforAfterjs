4f6b5fefb3fd3db02e7aa2c96ca0816b2a384f1d
ElmirQ <elmir.qasimzade@gmail.com>
2024-02-10 05:02:50 +0400

    end

--- js.js
+++ js.js
@@ -1,6 +1,7 @@
 const leman = document.getElementById('leman');
 function yandir() {
     leman.classList.add('lime');
+    document.getElementById('leman').innerHTML = "Salam Aleykum";
 }
 function sondur() {
     leman.classList.remove('lime');
@@ -8,4 +9,5 @@ function sondur() {
 
 function yandirSondur() {
     leman.classList.toggle('lime');
+    document.getElementById('leman').innerHTML = "Salam Aleykum";
 }
