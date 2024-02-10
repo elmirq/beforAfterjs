de174b05934c094343e8b5f7484ed915348f92e1
ElmirQ <elmir.qasimzade@gmail.com>
2024-02-10 04:22:55 +0400

    EQ

--- js.js
+++ js.js
@@ -1,13 +1,11 @@
-const box = document.getElementById('box');
-const buttons = document.querySelectorAll('button');
-// const circ = document.getElementsByClassName('circle');
-// const lime = document.querySelector('.lime');
-const lime = document.querySelectorAll('.lime');
-const circ = document.querySelectorAll('.circle');
-const wrapper = document.querySelector('.wrapper');
-const ziko = document.createElement('div');
-ziko.classList.add('wrapper')
-// lime[3].before(ziko);
-// // circ[1].remove();
-// circ[0].replaceWith(circ[2])
-wrapper.insertBefore(ziko, lime[1])
+const leman = document.getElementById('leman');
+function yandir() {
+    leman.classList.add('lime');
+}
+function sondur() {
+    leman.classList.remove('lime');
+}
+
+function yandirSondur() {
+    leman.classList.toggle('lime');
+}
